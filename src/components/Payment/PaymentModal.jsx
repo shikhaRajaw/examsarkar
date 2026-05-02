f2f9f1098ddc1d0653013c0e8498c78b46007144
Debojit <debojit.github@gmail.com>
2026-05-03 02:19:11 +0530

    Update PaymentModal.jsx

--- src/components/Payment/PaymentModal.jsx
+++ src/components/Payment/PaymentModal.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { buildApiUrl } from '../../utils/apiBaseUrl';
 
